7b9e5ab21d44a71d36257b709be6899a96464d8c
Umut Can Arda <47474804+FlySquare@users.noreply.github.com>
2020-11-26 04:04:08 +0300

    update

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const tmi = require("tmi.js");
-
+var db;
+var x1;
+var x2;
+var x3;
+var x4;
+var p1 = 0;
+var p2 = 0;
+var p3 = 0;
+var p4 = 0;
 const options = {
     options: {
       debug: true
@@ -22,9 +30,34 @@ const options = {
     client.action(chnl,'Doğruluk mu Cesaret miiiii???? Kappa ');
 }
 function random(){
-    return Math.floor(Math.random() * Math.floor(3));
+   db =  Math.floor(Math.random() * Math.floor(999));
+    if(db >= 499){
+        client.action(chnl,'DOĞRULUUUUUK!!');
+        setTimeout(questions,3000);
+    }
+    else
+    {
+        client.action(chnl,'CESAAAAAAAREEEET!!');
+        setTimeout(questions,3000);
+    }
     
+}
+function questions(){
+  if(db >=499){
+  
 
+    x1="A- Telefonunda en son aradığın şey neydi?";
+    x2="B- Yerden bir şeyi alıp hiç yedin mi?";
+    x3="C- Bir gün karşı cins olarak uyanırsan, ilk yapacağın şey nedir?";
+    x4="D- Hiç havuzda işedin mi?";
+    client.action(chnl,x1);
+    client.action(chnl,x2);
+    client.action(chnl,x3);
+    client.action(chnl,x4);
+  }else{
+      //
+  }
+    
 }
   
   // Edit channel name
@@ -38,11 +71,22 @@ function random(){
 client.on('connected', function (address, port) {
     startMessage();
     setTimeout(askQuestion, 3000);
-    client.action(chnl,random());
+    setTimeout(random,6000);
+   
 });
 
    client.on('chat', (channel, user, message, self) => {
-    if(message === "!facebook" || message === "!fb"){
-        client.action(chnl,'=> Facebook grubumuza katılarak her ortamdan bizden haberdar olabilirsin. https://www.facebook.com/ksinctwitchgrubu/');
+    if(message === "!sec A" || message === "!sec a"){
+        p1 = p1 + 1;
+        console.log(p1);
+    }else if(message === "!sec B" || message === "!sec b"){
+        p2 = p2 + 1;
+        console.log(p2);
+    }else if(message === "!sec C" || message === "!sec c"){
+        p3 = p3 + 1;
+        console.log(p3);
+    }else if(message === "!sec D" || message === "!sec d"){
+        p4 = p4 + 1;
+        console.log(p4);
     }
 });
